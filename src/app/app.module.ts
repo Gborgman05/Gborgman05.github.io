2eb4ec61d94d8e1270b0ebea69d36dbb375f41d5
Gborgman05 <gborgman@calpoly.edu>
2023-02-24 16:48:22 -0800

    tic-tac-toe tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { ContactComponent } from './contact/contact.component';
     HomeComponent,
     AboutComponent,
     ProjectsComponent,
-    ContactComponent
+    ContactComponent,
+    SquareComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
