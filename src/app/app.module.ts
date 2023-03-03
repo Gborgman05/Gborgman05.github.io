646d3bf22fa5791d1e40dd866df866779995b4c6
Gborgman05 <gborgman@calpoly.edu>
2023-03-03 09:33:08 -0800

    daily

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
@@ -14,7 +13,6 @@ import { BoardComponent } from './board/board.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PortfolioComponent,
     HomeComponent,
     AboutComponent,
     ProjectsComponent,
