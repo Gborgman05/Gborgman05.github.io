4d06f360ff8a513ba915d7c85bb89597b91130a0
Gborgman05 <gborgman@calpoly.edu>
2023-04-02 21:01:45 -0700

    daily

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,5 +9,8 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  @Input() desc = "I'm a learner focused on Software Engineering right now. \nI'm always reading something, you can see those here.\nFor some of my projects see here.\nIf you'd like to reach out go here.\n\nIf you want to play tictactoe (with yourself) go here. "
+  constructor() {
+  }
 
 }
