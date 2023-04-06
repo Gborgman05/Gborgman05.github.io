a9393b47abbbda1501f4e088f6f467cafd7a4a61
Gborgman05 <gborgman@calpoly.edu>
2023-04-06 15:35:38 -0700

    daily

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
   name = 'Galen Borgman';
-  profilePicture = 'https://example.com/your-profile-picture.jpg';
+  profilePicture = 'assets/profile.jpg';
   description = `I am an early-career software engineer with a strong passion for Angular and web technologies. My curiosity extends beyond the realm of web development, as I am also deeply interested in machine learning and its potential to revolutionize various industries. Alongside my technical pursuits, I have a deep appreciation for the art of poetry, which I believe enriches my creative thinking and problem-solving skills. Balancing my love for technology and the written word, I strive to craft innovative, efficient, and scalable solutions that make a difference.`;
 
   favoriteBooks = [
