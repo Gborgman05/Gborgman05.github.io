229320e9f959a333c1073c47afaa196cded55c8a
Gborgman05 <gborgman@calpoly.edu>
2023-04-12 14:02:06 -0700

    adding links and opening behavior on about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -11,17 +11,17 @@ export class AboutComponent {
   profilePicture = 'assets/profile.jpg';
   description = `I am an early-career software engineer with a strong passion for Angular and web technologies. My curiosity extends beyond the realm of web development, as I am also deeply interested in machine learning and its potential to revolutionize various industries. Alongside my technical pursuits, I have a deep appreciation for the art of poetry, which I believe enriches my creative thinking and problem-solving skills. Balancing my love for technology and the written word, I strive to craft innovative, efficient, and scalable solutions that make a difference.`;
   favorites: Favorite[] = [
-  { category: 'Book', name: 'Mistborn', link: '' },
-  { category: 'Book', name: 'The Way of Kings', link: '' },
-  { category: 'Book', name: 'Dune', link: '' },
+  { category: 'Book', name: 'Mistborn', link: 'https://www.brandonsanderson.com/the-mistborn-saga-the-original-trilogy/' },
+  { category: 'Book', name: 'The Way of Kings', link: 'https://www.brandonsanderson.com/the-stormlight-archive-series/' },
+  { category: 'Book', name: 'Dune', link: 'https://www.goodreads.com/book/show/44767458-dune' },
 
   { category: 'Hobby', name: 'Board Games', link: '' },
   { category: 'Hobby', name: 'Hiking', link: '' },
   { category: 'Hobby', name: 'Horror Movies', link: '' },
 
-  { category: 'Movie', name: 'Hereditary', link: '' },
-  { category: 'Movie', name: 'Inception', link: '' },
-  { category: 'Movie', name: 'The Secret Life of Walter Mitty', link: '' },
+  { category: 'Movie', name: 'Hereditary', link: 'https://www.imdb.com/title/tt7784604/' },
+  { category: 'Movie', name: 'Inception', link: 'https://www.imdb.com/title/tt1375666/' },
+  { category: 'Movie', name: 'The Secret Life of Walter Mitty', link: 'https://www.imdb.com/title/tt0359950/' },
 
   {
     category: 'Music',
@@ -39,9 +39,9 @@ export class AboutComponent {
     link: 'https://open.spotify.com/artist/2FXC3k01G6Gw61bmprjgqS',
   },
 
-  { category: 'Game', name: "Sid Meier's Civilization", link: '' },
-  { category: 'Game', name: 'Phasmophobia', link: '' },
-  { category: 'Game', name: 'Stardew Valley', link: '' },
+  { category: 'Game', name: "Sid Meier's Civilization", link: 'https://store.steampowered.com/app/289070/Sid_Meiers_Civilization_VI/' },
+  { category: 'Game', name: 'Phasmophobia', link: 'https://store.steampowered.com/app/739630/Phasmophobia/' },
+  { category: 'Game', name: 'Stardew Valley', link: 'https://store.steampowered.com/app/413150/Stardew_Valley/' },
 ];
   categoryLists= this.favorites.reduce((accumulator: any, item: Favorite) => {
     if (!accumulator[item.category]) {
