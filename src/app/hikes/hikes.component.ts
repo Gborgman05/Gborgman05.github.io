767e608b657088b4eda7ca1ac5b83a12c181c4a4
Gborgman05 <gborgman@calpoly.edu>
2023-04-16 23:17:55 -0700

    daily

---
+++ src/app/hikes/hikes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hikes',
+  templateUrl: './hikes.component.html',
+  styleUrls: ['./hikes.component.scss']
+})
+export class HikesComponent {
+
+}
