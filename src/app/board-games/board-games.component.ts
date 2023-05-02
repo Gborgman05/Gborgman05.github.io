63b093195e578872388ac96a206c5887af78f560
Gborgman05 <gborgman@calpoly.edu>
2023-05-01 23:40:36 -0700

    daily

--- src/app/board-games/board-games.component.ts
+++ src/app/board-games/board-games.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Favorite } from '../about/favorite.model';
 
 @Component({
   selector: 'app-board-games',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./board-games.component.scss']
 })
 export class BoardGamesComponent {
-
+  favoriteBG:Favorite[] = [
+  { category: 'Board Game', name: 'Spirit Island', link: 'https://boardgamegeek.com/boardgame/162886/spirit-island' },
+  { category: 'Board Game', name: 'Arkham Horror: The Card Game', link: 'https://boardgamegeek.com/boardgame/205637/arkham-horror-card-game' },
+  { category: 'Board Game', name: 'Gloomhaven', link: 'https://boardgamegeek.com/boardgame/174430/gloomhaven' },
+]
 }
