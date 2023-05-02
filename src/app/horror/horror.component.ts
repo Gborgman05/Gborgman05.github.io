fc5709e303f7b6bafff8aaa896b88cb1442edbc5
Gborgman05 <gborgman@calpoly.edu>
2023-05-01 23:36:04 -0700

    daily

--- src/app/horror/horror.component.ts
+++ src/app/horror/horror.component.ts
@@ -11,8 +11,8 @@ export class HorrorComponent {
   { category: 'Movie', name: 'Hereditary', link: 'https://www.imdb.com/title/tt7784604/' },
   { category: 'Podcast', name: 'The Magnus Archive', link: 'https://rustyquill.com/show/the-magnus-archives/' },
   { category: 'TV Series', name: 'The Haunting of Hill House', link: 'https://en.wikipedia.org/wiki/The_Haunting_of_Hill_House_(TV_series)' },
-  { category: 'Movie', name: 'The Conjuring', link: 'https://en.wikipedia.org/wiki/The_Haunting_of_Hill_House_(TV_series)' },
-  { category: 'TV Series', name: '', link: 'https://en.wikipedia.org/wiki/The_Haunting_of_Hill_House_(TV_series)' },
+  { category: 'Movie', name: 'The Conjuring', link: 'https://www.imdb.com/title/tt1457767/' },
+  { category: 'TV Series', name: 'American Horror Story: 1984', link: 'https://en.wikipedia.org/wiki/American_Horror_Story:_1984' },
 ]
 
 
